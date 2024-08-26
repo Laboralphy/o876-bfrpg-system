e9b92a6454635fa6105f0ed06edf9a5dc490050d
Raphael Marandet <raphael.marandet@gmail.com>
2024-08-26 08:50:04 +0200

    stealth will not be subject of periodic skill rolls

--- src/store/getters/getCapabilities.js
+++ src/store/getters/getCapabilities.js
@@ -8,25 +8,27 @@ const CONSTS = require('../../consts')
  */
 module.exports = (state, getters) => {
     const aConditionSet = getters.getConditionSet
-    const act = !(
+    const alive = !getters.isDead
+    const act = alive && !(
         aConditionSet.has(CONSTS.CONDITION_STUNNED) ||
         aConditionSet.has(CONSTS.CONDITION_PARALYZED) ||
         aConditionSet.has(CONSTS.CONDITION_PETRIFIED) ||
         aConditionSet.has(CONSTS.CONDITION_INCAPACITATED)
     )
-    const fight = act && !aConditionSet.has(CONSTS.CONDITION_DAZED)
-    const move = act && aConditionSet.has(CONSTS.CONDITION_RESTRAINED)
-    const see = !aConditionSet.has(CONSTS.CONDITION_BLINDED)
-    const castSelf = act && !aConditionSet.has(CONSTS.CONDITION_DAZED)
-    const castTarget = castSelf && see
+    const fight = alive && act && !aConditionSet.has(CONSTS.CONDITION_DAZED)
+    const move = alive && act && aConditionSet.has(CONSTS.CONDITION_RESTRAINED)
+    const see = alive && !aConditionSet.has(CONSTS.CONDITION_BLINDED)
+    const castSelf = alive && act && !aConditionSet.has(CONSTS.CONDITION_DAZED)
+    const castTarget = alive && castSelf && see
     const cast = {
         self: castSelf,
         target: castTarget
     }
     const sClassTypeRef = getters.getClassTypeData.ref
-    const sneak =
+    const sneak = alive && (
         sClassTypeRef === CONSTS.CLASS_TYPE_ROGUE ||
         sClassTypeRef === CONSTS.CLASS_TYPE_ROGUE_MAGIC_USER
+    )
     return {
         act,
         move,
