ad708968874fdf3f5d901acacbe13ed8734cc8f1
ralphy <raphael.marandet@gmail.com>
2024-08-30 12:17:11 +0200

    version 0.11.2 daze is cancelled when attacked

--- src/effects/daze.js
+++ src/effects/daze.js
@@ -1,2 +1,10 @@
+function attacked ({ effect, target, attackOutcome }) {
+    // effect is over
+    if (attackOutcome.hit && attackOutcome.damages.amount > 0) {
+        target.mutations.removeEffect({ effect })
+    }
+}
+
 module.exports = {
+    attacked
 }
